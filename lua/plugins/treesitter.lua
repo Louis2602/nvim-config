4a6c58b2a084b2914c3ac2a7cc75fde1ec19097b
Lam Tran <ttlam.dev@gmail.com>
2024-06-28 19:24:44 +0700

    fix bug

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -34,11 +34,11 @@ return {
 		dependencies = "nvim-treesitter/nvim-treesitter",
 		config = function()
 			require("nvim-ts-autotag").setup({
-        opts = {
-          enable_close = true,
-          enable_rename = true,
-          enable_close_on_slash = true
-        }
+				opts = {
+					enable_close = true,
+					enable_rename = true,
+					enable_close_on_slash = true,
+				},
 				filetypes = {
 					"html",
 					"javascript",
