e490b887ba11fb6f5b4a4ce964dda8b5c4da0187
Lam Tran <lam.tran@tecalliance.net>
2024-06-28 11:30:29 +0700

    update formatter

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.coding.copilot" },
 		{ import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
+		{ import = "lazyvim.plugins.extras.lsp.none-ls" },
 		{ import = "plugins" },
 	},
 	defaults = {
