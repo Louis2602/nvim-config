96fcb507db69f1e8c45a1f8eac2ef62edd15cb2f
Lam Tran <ttlam.dev@gmail.com>
2024-08-06 21:25:49 +0700

    update floating window color

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -23,3 +23,10 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     vim.api.nvim_set_hl(0, "Visual", { bg = "#3c3836", fg = "NONE" })
   end,
 })
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#191D24" })
+  end,
+})
