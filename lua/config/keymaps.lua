fd7c2b6ce04d31f0f22d65e376a07e67fb008366
Lam Tran <lam.tran@tecalliance.net>
2024-08-14 14:12:31 +0700

    update navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -36,10 +36,10 @@ keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
 
 -- Move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
+keymap.set("n", "sh", "<C-h>")
+keymap.set("n", "sk", "<C-k>")
+keymap.set("n", "sj", "<C-j>")
+keymap.set("n", "sl", "<C-l>")
 
 -- Resize window
 keymap.set("n", "<C-S-h>", "<C-w><")
