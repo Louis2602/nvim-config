bcfcc076526a08a63c349e2338406d710e498085
Tùng Lâm Trần <ttlam.dev@gmail.com>
2024-08-29 10:11:27 +0700

    remove elixir dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -42,26 +42,6 @@ return {
       --   },
       -- }
 
-      local elixir_ls_debugger = vim.fn.exepath("elixir-ls-debugger")
-      if elixir_ls_debugger ~= "" then
-        dap.adapters.mix_task = {
-          type = "executable",
-          command = elixir_ls_debugger,
-        }
-
-        dap.configurations.elixir = {
-          {
-            type = "mix_task",
-            name = "phoenix server",
-            task = "phx.server",
-            request = "launch",
-            projectDir = "${workspaceFolder}",
-            exitAfterTaskReturns = false,
-            debugAutoInterpretAllModules = false,
-          },
-        }
-      end
-
       vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
       vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
 
